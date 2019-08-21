9223163979d9cc389671b14a87ffffb71369a8f5
jiangyu <jiangyuwise@gmail.com>
2019-08-21 14:24:55 +0800

    添加安卓版本检测

--- app/src/main/java/com/codve/geoquiz/CheatActivity.java
+++ app/src/main/java/com/codve/geoquiz/CheatActivity.java
@@ -2,10 +2,14 @@ package com.codve.geoquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -40,9 +44,27 @@ public class CheatActivity extends AppCompatActivity {
                     mAnswerTextView.setText(R.string.false_button);
                 }
                 setAnswerShowResult(true); // 这一步打包数据
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswerButton.getWidth() / 2;
+                    int cy = mShowAnswerButton.getHeight() / 2;
+                    float radius = mShowAnswerButton.getWidth();
+                    Animator animator = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
+                    animator.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd (Animator animation){
+                            super.onAnimationEnd(animation);
+                            mShowAnswerButton.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    animator.start();
+                } else {
+                    mShowAnswerButton.setVisibility(View.INVISIBLE);
+                }
+
             }
         });
-
     }
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
